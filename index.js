87ddf6e12ff6fdee737482a3e5f9667f6a41a634
Abu Motaleb <abumotaleb99@gmail.com>
2023-05-21 20:04:24 +0600

    created api to get single toy by id

--- index.js
+++ index.js
@@ -78,6 +78,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/view-toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
+
     app.get("/toy/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
